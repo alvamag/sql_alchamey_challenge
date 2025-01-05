4f47efc958e490d7d395abac7d49ec6d8ad2aaeb
alvamag <ma210@rice.edu>
2025-01-05 16:35:20 -0600

    module 10 challenge

---
+++ sql_alchamey challenge/Starter_Code/.ipynb_checkpoints/app-checkpoint.py
@@ -0,0 +1,37 @@
+# Import the dependencies.
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+if __name__ == '__main__':
+    app.run()
+
+
+
+
+#################################################
+# Database Setup
+#################################################
+
+
+# reflect an existing database into a new model
+
+# reflect the tables
+
+
+# Save references to each table
+
+
+# Create our session (link) from Python to the DB
+
+
+#################################################
+# Flask Setup
+#################################################
+
+
+
+
+#################################################
+# Flask Routes
+#################################################
